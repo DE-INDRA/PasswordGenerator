bdb1d7f7b9689d80af55f97890ff01912251c5b8
INDRANIL DE <indranil.de.2001@gmail.com>
2024-03-16 00:42:22 +0530

    re-modification

--- practise.js
+++ practise.js
@@ -1,14 +1,19 @@
 
 const stock=["abcdefghijklmnopqrstuvwxyz","ABCDEFGHIJKLMNOPQRSTUVWXYZ","1234567890","@#$%&*/()?><"];
  //Enter the length of thwe password to be generated...
+ const trigger=document.getElementById("trigger");
+ const rangeval=document.getElementById("size");
+ rangeval.addEventListener("input", ()=>{
+    document.getElementById("lengthvalue").textContent=rangeval.value;
+ });
 function generate(){
     let answer="";
-    const length=parseInt(document.getElementById("length").value);
-    const islowercase=document.getElementById("lower-select").value;
-    const isuppercase=document.getElementById("upper-select").value;
-    const isnumber=document.getElementById("number-select").value;
-    const ischaracter=document.getElementById("character-select").value;
-    if (islowercase === "Select" || isuppercase === "Select" || isnumber === "Select" || ischaracter === "Select") {
+    const length=parseInt(document.getElementById("size").value);
+    const islowercase=document.getElementById("lowercase").checked;
+    const isuppercase=document.getElementById("uppercase").checked;
+    const isnumber=document.getElementById("numbers").checked;
+    const ischaracter=document.getElementById("characters").checked;
+    if (!islowercase && !isuppercase && !isnumber && !ischaracter) {
         alert("Please select options for all criteria.");
         return;
     }
@@ -26,10 +31,10 @@ function generate(){
             answer+=char;
         }
     }
-    document.getElementById("screen").value=answer;
+    document.getElementById("result").textContent=answer;
 }
 function find(flag,num){
-    if(flag=="True"){
+    if(flag){
         let max=stock[num-1].length;
         let random=Math.floor(Math.random()*max);
         return stock[num-1].charAt(random);
@@ -38,3 +43,31 @@ function find(flag,num){
         return -1;
     }
 }
+trigger.addEventListener("click",generate);
+
+
+const copyButton = document.getElementById("copy");
+copyButton.addEventListener("click", function() {
+    
+    const resultSpan = document.getElementById("result");
+    // Create a temporary textarea element
+    const tempTextarea = document.createElement("textarea");
+
+    // Set the value of the textarea to the text content of the result span
+    tempTextarea.value = resultSpan.textContent;
+
+    // Append the textarea to the document body
+    document.body.appendChild(tempTextarea);
+
+    // Select the content of the textarea
+    tempTextarea.select();
+
+    // Execute the copy command to copy the selected text to the clipboard
+    document.execCommand("copy");
+
+    // Remove the temporary textarea from the document body
+    document.body.removeChild(tempTextarea);
+
+    // Alert the user that the content has been copied
+    alert("Copied to clipboard!");
+});
